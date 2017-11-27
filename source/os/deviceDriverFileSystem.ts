c12c428496a121537f59f7708fa21aa3b85bb01a
nisiya <syni679@gmail.com>
2017-11-27 01:04:08 -0500

    working on file system driver

---
+++ source/os/deviceDriverFileSystem.ts
@@ -0,0 +1,22 @@
+///<reference path="../globals.ts" />
+///<reference path="deviceDriver.ts" />
+
+/* ----------------------------------
+   DeviceDriverFileSystem.ts
+
+   Requires deviceDriver.ts
+
+   The File System Device Driver.
+   ---------------------------------- */
+
+   module TSOS {
+    
+        // Extends DeviceDriver
+        export class DeviceDriverFileSystem extends DeviceDriver {
+    
+            constructor() {
+            }
+    
+        }
+    }
+    
